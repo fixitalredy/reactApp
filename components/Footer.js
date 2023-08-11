a1b64789b0d77b0a4cfe7a1503fa96ddc20f32b3
Ermakov Nikita <vienn1009@gmail.com>
2023-08-11 17:43:11 +0300

    Updates

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import TaskFilter from "./TaskFilter";
+
+export default function Footer() {
+  return (
+  <footer className="footer">
+    <span className="todo-count">1 items left</span>
+    <TaskFilter />
+    <button className="clear-completed">Clear completed</button>
+  </footer>) ;
+}
---
+++ components/Task.js
@@ -0,0 +1,18 @@
+export default function Task(props) {
+  return (
+    <li className={props.className ? props.className : null}>
+      <div className="view">
+        <input className="toggle" type="checkbox" />
+        <label>
+          <span className="description">{props.value}</span>
+          <span className="created">{props.date}</span>
+        </label>
+        <button className="icon icon-edit"></button>
+        <button className="icon icon-destroy"></button>
+      </div>
+      {props.className === "editing" ? (
+        <input type="text" className="edit" defaultValue={props.value} />
+      ) : null}
+    </li>
+  );
+}
