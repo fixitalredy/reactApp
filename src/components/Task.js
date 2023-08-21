a4644732122b2b334d16517e30c32bc77b1f0353
Ermakov Nikita <vienn1009@gmail.com>
2023-08-21 15:23:27 +0300

    test husky

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import PropTypes from 'prop-types';
 
